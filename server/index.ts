44b030ca8fa1493178d0427a9a00f2ea474f599e
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-12-15 12:38:47 -0300

    connecting server with database

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
-const app = express();
-const PORT = 8000;
-app.get("/", (req, res) => res.send("iMentor server is up and running"));
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
+import mongoose from "mongoose";
+
+// Connect to MongoDB database
+mongoose
+  .connect("mongodb://localhost:27017/players", { useNewUrlParser: true })
+  .then(() => {
+    const app = express();
+    const PORT = 8000;
+    app.get("/", (req, res) => res.send("iMentor server is up and running"));
+    app.listen(PORT, () => {
+      console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+    });
+  });
